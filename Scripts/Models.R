46dab7e648291878dd2d3b3a811913a0c3f4d51a
sab3r-ncirl <x19149140@student.ncirl.ie>
2020-04-25 17:47:57 +0100

    final report updates

--- Scripts/Models.R
+++ Scripts/Models.R
@@ -42,6 +42,13 @@ accidents_df$Rest.Update <- NULL
 accidents_df$District <- NULL
 accidents_df$Environment <- NULL
 accidents_df$Roadway <- NULL
+accidents_df$County <- NULL
+accidents_df$Route <- NULL
+accidents_df$Crash.Day <- NULL
+accidents_df$Crash.Time <- NULL
+accidents_df$Route <- NULL
+accidents_df$Crash.Location <- NULL
+
 
 install.packages('caTools')
 
@@ -49,6 +56,18 @@ install.packages('caTools')
 # Encode target variable as factor
 
 accidents_df$Fatality <- as.factor(accidents_df$Fatality)
+accidents_df_backup <- accidents_df
+class(accidents_df_backup$Occupants)
+
+
+# Remove rows where occupants > 30 as these might be errors
+accidents_df <- accidents_df_backup[accidents_df_backup$Occupants < 31,]
+boxplot(accidents_df$Occupants)
+
+######################
+## Train Test Split ##
+######################
+
 
 
 require(caTools)  # loading caTools library
@@ -58,11 +77,16 @@ sample = sample.split(accidents_df,SplitRatio = 0.75) # splits the data in the r
 train1 =subset(accidents_df,sample ==TRUE) # creates a training dataset named train1 with rows which are marked as TRUE
 test1=subset(accidents_df, sample==FALSE)
 
-logistic_model <- glm(Fatality ~Major.Cause+Crash.Manner+Surface.Conditions+Drug.Alcohol.Related+Light.Conditions+Light.Conditions+Weather.Conditions+Vehicles+Occupants+Property.Damage,family=binomial(link='logit'),data=train1, weights = weight)
+#########################
+## Logistic Regression ##
+#########################
+
+
+logistic_model <- glm(Fatality ~Major.Cause+Crash.Manner+Surface.Conditions+Drug.Alcohol.Related+Light.Conditions+Light.Conditions+Weather.Conditions+Vehicles+Occupants+Property.Damage,family=binomial(link='logit'),data=train1)
 
 
 summary(logistic_model)
-
+library(caret)
 
 
 fitted.results.probability <- predict(logistic_model,newdata=test1,type='response')
@@ -72,16 +96,30 @@ print(paste('Accuracy',1-misClasificError))
 
 fitted.results
 
+confusionMatrix(table(test1$Fatality, fitted.results), mode = "everything", positive = "1")
+
 cm = table(test1$Fatality, fitted.results)
 
 cm
 
-weight <- train1$Fatality
-weight <- weight *29
-weight <- weight + 1
-table(weight)
+logistic_recall <- diag(cm) / rowSums(cm)
+logistic_recall
 
-# plotting logistic regression 
+
+# ROC plot and F1 score
+
+#install.packages('ROCR')
+library(ROCR)
+p <- predict(logistic_model, newdata=test1, type="response")
+pr <- prediction(p, test1$Fatality)
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
+
+# F1 score using ROCR
+logistic_f1_score <- performance(pr,"f")
+logistic_f1_score
+
+# Plotting logistic regression 
 
 predicted.data <- data.frame(probability.of.fatality=fitted.results.probability, fatality=test1$Fatality)
 predicted.data <- predicted.data[order(predicted.data$probability.of.fatality, decreasing = FALSE), ]
@@ -94,17 +132,81 @@ ggplot(data = predicted.data, aes(x=rank, y=probability.of.fatality)) +
   xlab("Index") +
   ylab("Predicted probability of fatality")
 
-ggsave()
+#ggsave()
+
+
+#######################################
+### Logistic Regression with Weights ##
+#######################################
+
+
+# Assigning weights for the dependent variable in the training dataset
+
+table(train1$Fatality)
+
+weight <- as.numeric(as.character(train1$Fatality))
+weight <- weight *29
+weight <- weight + 1
+
+###############################################################
+## Weight of a fatality is 30 times more than a non-fatality ##
+###############################################################
+
+table(weight)
+
+weighted_logistic_model <- glm(Fatality ~Major.Cause+Crash.Manner+Surface.Conditions+Drug.Alcohol.Related+Light.Conditions+Light.Conditions+Weather.Conditions+Vehicles+Occupants+Property.Damage,family=binomial(link='logit'),data=train1, weights = weight)
+
+varImp(weighted_logistic_model)
+summary(weighted_logistic_model)
+
+
+
+weighted_fitted.results.probability <- predict(weighted_logistic_model,newdata=test1,type='response')
+weighted_fitted.results <- ifelse(weighted_fitted.results.probability > 0.4,1,0)
+weighted_misClasificError <- mean(weighted_fitted.results != test1$Fatality)
+print(paste('Accuracy',1-weighted_misClasificError))
+
+weighted_fitted.results
+
+table(test1$Fatality)
+confusionMatrix(table(test1$Fatality, weighted_fitted.results), mode="everything", positive = "1")
+weighted_cm = table(test1$Fatality, weighted_fitted.results)
+
+weighted_cm
+
+## Checking Recall for this model
+
+weighted_logistic_recall <- diag(weighted_cm) / rowSums(weighted_cm)
+weighted_logistic_recall
+
+# Plotting logistic regression 
+
+weighted_predicted.data <- data.frame(probability.of.fatality=weighted_fitted.results.probability, fatality=test1$Fatality)
+weighted_predicted.data <- predicted.data[order(weighted_predicted.data$probability.of.fatality, decreasing = FALSE), ]
+weighted_predicted.data$rank <- 1:nrow(weighted_predicted.data)
+library(ggplot2)
+library(cowplot)
+
+ggplot(data = weighted_predicted.data, aes(x=rank, y=probability.of.fatality)) +
+  geom_point(aes(color=fatality), alpha=1, shape=4, stroke=2) +
+  xlab("Index") +
+  ylab("Predicted probability of fatality")
+
+#ggsave()
 
 #table(train1$Fatality, fitted.results > 0.5)
 #names(fitted.results)
 
-install.packages('ROCR')
+#install.packages('ROCR')
 library(ROCR)
-p <- predict(model, newdata=test1, type="response")
-pr <- prediction(p, test1$Fatality)
-prf <- performance(pr, measure = "tpr", x.measure = "fpr")
-plot(prf)
+weighted_p <- predict(weighted_logistic_model, newdata=test1, type="response")
+weighted_pr <- prediction(weighted_p, test1$Fatality)
+weighted_prf <- performance(weighted_pr, measure = "tpr", x.measure = "fpr")
+plot(weighted_prf)
+
+# F1 score using ROCR
+weighted_logistic_f1_score <- performance(pr,"f")
+weighted_logistic_f1_score
 
 #auc <- performance(pr, measure = "auc")
 #auc <- auc@y.values[[1]]
@@ -113,27 +215,79 @@ plot(prf)
 
 
 
+############################
+## Naive-Bayes Classifier ##
+############################
 
-# naive bayes
+
+require(caTools)  # loading caTools library
+
+## Train Test Split
+
+set.seed(123)   #  set seed to ensure you always have same random numbers generated
+sample = sample.split(accidents_df,SplitRatio = 0.75) # splits the data in the ratio mentioned in SplitRatio. After splitting marks these rows as logical TRUE and the the remaining are marked as logical FALSE
+train1 =subset(accidents_df,sample ==TRUE) # creates a training dataset named train1 with rows which are marked as TRUE
+test1=subset(accidents_df, sample==FALSE)
 
 #install.packages('e1071')
 library('e1071')
 
-classifier = naiveBayes(x = train1[-26], y = train1$Fatality)
+## Training the classifier
 
-naive_pred = predict(classifier, newdata = test1[-26])
+class(train1$Fatality)
+train1$Fatality <- as.factor(train1$Fatality)
+test1$Fatality <- as.factor(test1$Fatality)
+
+str(train1)
+#classifier = naiveBayes(x = train1[-38], y = train1$Fatality)
+str(train1[-21])
+classifier = naiveBayes(x = train1[-21], y = train1$Fatality)
+
+
+
+library(caret)
+#varImp(classifier)
+
+summary(classifier)
+
+# Predicting values in the test dataset
+
+naive_pred = predict(classifier, newdata = test1[-21])
+#View(naive_pred)
 naive_cm = table(test1$Fatality, naive_pred)
 naive_cm
+confusionMatrix(table(test1$Fatality, naive_pred), mode="everything", positive = "1")
+#recall(table(test1$Fatality, naive_pred), relevant = levels(test1$Fatality)[2])
+levels(test1$Fatality)
+
+## Checking the Recall in Naive Bayes classifier
+
+naive_recall <- diag(naive_cm) / colSums(naive_cm)
+naive_recall
+
+# plot naive bayes confusion matrix
+plot(naive_cm)
+
+# Plotting ROC for Naive Bayes
+
+p_naive <- predict(classifier, newdata=test1[-38], type="raw")
+class(p_naive)
+table(p_naive)
+p_naive[,2]
+class(test1$Fatality)
+pr_naive <- prediction(p_naive[,2], test1$Fatality)
+prf_naive <- performance(pr_naive, measure = "tpr", x.measure = "fpr")
+plot(prf_naive)
 
 
-# visualise
-plot(classifier)
 
 
 
+#######################################
+## Support Vector Machine Classifier ##
+#######################################
 
-
-#svm
+## Both Linear and Radial SVM models are implemented
 
 library(e1071)
 svm_classifier_radial = svm(formula = Fatality ~Major.Cause+Crash.Manner+Surface.Conditions+Drug.Alcohol.Related+Light.Conditions+Light.Conditions+Weather.Conditions+Vehicles+Occupants+Property.Damage, 
@@ -147,11 +301,11 @@ svm_classifier_linear = svm(formula = Fatality ~Major.Cause+Crash.Manner+Surface
                             kernel = 'linear')
 
 
-plot(svm_classifier_linear, train1['Light.Conditions', 'Weather.Conditions', 'Fatality'])
+#plot(svm_classifier_linear, train1['Light.Conditions', 'Weather.Conditions', 'Fatality'])
 
-y_pred_svm_radial = predict(svm_classifier_radial, newdata = test1[-26])
+y_pred_svm_radial = predict(svm_classifier_radial, newdata = test1[-21])
 
-y_pred_svm_linear = predict(svm_classifier_linear, newdata = test1[-26])
+y_pred_svm_linear = predict(svm_classifier_linear, newdata = test1[-21])
 
 cm_svm_radial = table(test1$Fatality, y_pred_svm_radial)
 
@@ -161,11 +315,34 @@ cm_svm_radial
 cm_svm_linear
 
 
+# Confustion Matrix and Recall
+caret_cm_svm_linear <- confusionMatrix(table(test1$Fatality, y_pred_svm_linear), mode = "everything", positive = "1")
+caret_cm_svm_linear
+
+# SVM Recall for Radial Model
+svm_recall <- diag(cm_svm_radial) / rowSums(cm_svm_radial)
+svm_recall
+
+# SVM Recall for Linear Model
+svm_recall <- diag(cm_svm_linear) / rowSums(cm_svm_linear)
+svm_recall
+
+
+# plot ROC for svm
+
+p_svm <- predict(svm_classifier_linear, newdata=test1, type="response")
+class(p_svm)
+table(p_svm)
+p_svm <- as.integer(p_svm)
+class(test1$Fatality)
+pr_svm <- prediction(p_svm, test1$Fatality)
+prf_svm <- performance(pr_svm, measure = "tpr", x.measure = "fpr")
+plot(prf_svm)
 
 
 # Metrics for evaluating the fit of the models (Recall)
-recall <- diag(naive_cm) / rowSums(naive_cm)
-recall
+naive_bayes_recall <- diag(naive_cm) / rowSums(naive_cm)
+naive_bayes_recall
 
 
 
@@ -173,8 +350,9 @@ recall
 
 
 
-
-
+###############################
+## Alcohol and Climate Study ##
+###############################
 
 
 alcohol_df <- read.csv("C:\\Users\\Soham More\\Documents\\GitHub\\dmml1\\Datasets\\final_merged_alcohol_weather_with_0_prcp_temp_cleaned.csv", header = TRUE)
@@ -206,9 +384,14 @@ test_set = subset(x, split == FALSE)
 library(corrplot)
 head(training_set)
 cor_mat <- cor(training_set[, c(3,4,8,9,10,11,12,13)], method = c('pearson'))
-corrplot(cor_mat, method='circle', tl.srt=45)
+corrplot(cor_mat, method='number')
 
-
+### Check outliers
+boxplot(training_set$SNWD)
+boxplot(training_set$SNOW)
+boxplot(training_set$TMIN)
+boxplot(training_set$TMAX)
+boxplot(training_set$PRCP)
 
 ### Create multiple linear model
 
@@ -246,22 +429,144 @@ cv = lapply(folds, function(x) {
   test_fold = alcohol_df_without_NA
   regressor = lm(formula = Sale..Dollars. ~ PRCP + AWND + SNOW + SNWD + TMAX + TMIN,
                  data = training_fold)
-  #classifier = svm(formula = Purchased ~ .,
-  #                 data = training_fold,
-  #                 type = 'C-classification',
-  #                 kernel = 'radial')
   y_pred = predict(regressor, newdata = test_fold[-3])
-  #cm = table(test_fold[, 3], y_pred)
   rsquare = R2(y_pred, test_fold$Sale..Dollars.)
   return(rsquare)
 })
 rsquare_with_k_fold = mean(as.numeric(cv))
 rsquare_with_k_fold
 
+## RMSE
+
+folds = createFolds(alcohol_df_without_NA$Sale..Dollars., k = 10)
+cv = lapply(folds, function(x) {
+  training_fold = alcohol_df_without_NA[-x, ]
+  test_fold = alcohol_df_without_NA
+  regressor = lm(formula = Sale..Dollars. ~ PRCP + AWND + SNOW + SNWD + TMAX + TMIN,
+                 data = training_fold)
+  y_pred = predict(regressor, newdata = test_fold[-3])
+  rsquare = RMSE(y_pred, test_fold$Sale..Dollars.)
+  return(rsquare)
+})
+rmse_with_k_fold = mean(as.numeric(cv))
+rmse_with_k_fold
 
 
 
-# SVR
+
+#### Ridge regression 
+
+library(glmnet)
+
+independent_vars <- as.matrix(training_set[, c(8,9,10,11,12,13)])
+dependent_vars <- training_set$Sale..Dollars.
+lambda_seq <- 10^seq(2, -2, by = -.1)
+#lambda_seq <- as.numeric(unlist(lambda_seq))
+#dependent_vars <- as.numeric(unlist(dependent_vars))
+
+
+fit <- glmnet(independent_vars, dependent_vars, alpha = 0, lambda  = lambda_seq)
+summary(fit)
+ridge_cv <- cv.glmnet(independent_vars, dependent_vars, alpha = 0, lambda = lambda_seq)
+
+# identifying best lamda
+
+best_lambda <- ridge_cv$lambda.min
+best_lambda
+best_fit <- ridge_cv$glmnet.fit
+head(best_fit)
+
+# Rebuilding the model with best lamda value identified
+best_ridge <- glmnet(independent_vars, dependent_vars, alpha = 0, lambda = 100)
+coef(best_ridge)
+
+# testing
+
+test_independent_vars <- as.matrix(test_set[, c(8,9,10,11,12,13)])
+
+pred <- predict(best_ridge, s = best_lambda, newx = test_independent_vars)
+actual_sales <- test_set$Sale..Dollars.
+R2(pred, actual_sales)
+RMSE(pred, actual_sales)
+
+
+
+
+
+# Lasso regression
+
+lasso_x_vars <- as.matrix(training_set[, c(8,9,10,11,12,13)])
+lasso_y_var <- training_set$Sale..Dollars.
+lambda_seq <- 10^seq(2, -2, by = -.1)
+
+
+# Splitting the data into test and train
+set.seed(86)
+train = sample(1:nrow(lasso_x_vars), nrow(lasso_x_vars)/2)
+x_test = (-train)
+y_test = lasso_y_var[test]
+
+cv_output <- cv.glmnet(lasso_x_vars[train,], lasso_y_var[train],
+                       alpha = 1, lambda = lambda_seq)
+
+# identifying best lamda
+best_lam <- cv_output$lambda.min
+best_lam
+
+
+# Rebuilding the model with best lamda value identified
+lasso_best <- glmnet(lasso_x_vars[train,], lasso_y_var[train], alpha = 1, lambda = best_lam)
+lasso_pred <- predict(lasso_best, s = best_lam, newx = lasso_x_vars[test,])
+
+# Finally, we combine the predicted values and actual values to see the two values side by side and then you can use the 
+# R-Squared formula to check the model performance. Note - you must calculate the R-Squared values for both train and test dataset.
+
+final <- cbind(lasso_y_var[test], lasso_pred)
+# Checking the first six obs
+head(final)
+R2(lasso_y_var[test], lasso_pred)
+RMSE(lasso_pred, lasso_y_var[test])
+
+
+
+# Elastic Net regression
+
+elastic_x_vars <- as.matrix(training_set[, c(8,9,10,11,12,13)])
+elastic_y_var <- training_set$Sale..Dollars.
+lambda_seq <- 10^seq(2, -2, by = -.1)
+
+
+# Splitting the data into test and train
+set.seed(86)
+train = sample(1:nrow(elastic_x_vars), nrow(elastic_x_vars)/2)
+elastic_net_x_test = (-train)
+elastic_net_y_test = elastic_y_var[test]
+
+elastic_cv_output <- cv.glmnet(elastic_x_vars[train,], elastic_y_var[train],
+                               alpha = 1, lambda = lambda_seq)
+
+# identifying best lamda
+best_lam <- elastic_cv_output$lambda.min
+best_lam
+
+
+# Rebuilding the model with best lamda value identified
+elastic_best <- glmnet(elastic_x_vars[train,], elastic_y_var[train], alpha = 0.5, lambda = best_lam)
+elastic_pred <- predict(elastic_best, s = best_lam, newx = elastic_x_vars[test,])
+
+# Finally, we combine the predicted values and actual values to see the two values side by side and then you can use the 
+# R-Squared formula to check the model performance. Note - you must calculate the R-Squared values for both train and test dataset.
+
+final <- cbind(elastic_y_var[test], elastic_pred)
+# Checking the first six obs
+head(final)
+RMSE(elastic_pred, elastic_y_var[test])
+R2(elastic_y_var[test], elastic_pred)
+
+
+#########
+## SVR ##
+#########
 
 library(e1071)
 library(caret)
@@ -274,3 +579,12 @@ svr_y_pred <- predict(svr_model, test_set)
 RMSE(svr_y_pred, test_set$Sale..Dollars.)
 # (b) R-square
 R2(svr_y_pred, test_set$Sale..Dollars.)
+
+
+
+
+
+
+
+
+
--- Scripts/Plotting Data on Maps.R
+++ Scripts/Plotting Data on Maps.R
@@ -11,18 +11,37 @@ world <- ne_countries(scale = "medium", returnclass = "sf")
 class(world)
 head(world)
 
-accidents_df <- read.csv('C:/Users/Soham More/Documents/Github/dmml1/Datasets/Iowa_Road_Accidents/v1_Data_Processing/Vehicle_Accidents_in_Iowa_by_Location__Last_Ten_Years_.csv', header = TRUE)
+accidents_df_for_plotting <- read.csv('C:/Users/Soham More/Documents/Github/dmml1/Datasets/Iowa_Road_Accidents/v1_Data_Processing/Road_Accidents_2015_2016.csv', header = TRUE)
 
-accident_location <- accidents_df$Crash.Location
-accident_location
-accident_location <- sub("\\).*", "", sub(".*\\(", "", accident_location)) 
-accident_location
-accident_location_df <- data.frame(do.call('rbind', strsplit(as.character(accident_location),' ',fixed=TRUE)))
-accident_location_df
-colnames(accident_location_df) <- c('longitude', 'latitude')
+fatality_locations <- accidents_df_for_plotting$Crash.Location[accidents_df$Fatalities>0]
+fatality_locations <- sub("\\).*", "", sub(".*\\(", "", fatality_locations)) 
+fatality_locations <- data.frame(do.call('rbind', strsplit(as.character(fatality_locations),' ',fixed=TRUE)))
+tail(fatality_locations)
+colnames(fatality_locations) <- c('longitude', 'latitude')
+str(fatality_locations)
+fatality_locations$longitude <- as.numeric(as.character(fatality_locations$longitude))
+fatality_locations$latitude <- as.numeric(as.character(fatality_locations$latitude))
+(fatality_locations_sf <- st_as_sf(fatality_locations, coords = c("longitude", "latitude"), 
+                                  crs = 4326, agr = "constant"))
 
-(accident_location_df <- st_as_sf(accident_location_df, coords = c("longitude", "latitude"), 
-                   crs = 4326, agr = "constant"))
+
+#accident_location <- accidents_df$Crash.Location
+#accident_location
+#accident_location <- sub("\\).*", "", sub(".*\\(", "", accident_location)) 
+#accident_location
+#accident_location_df <- data.frame(do.call('rbind', strsplit(as.character(accident_location),' ',fixed=TRUE)))
+#tail(accident_location_df)
+#colnames(accident_location_df) <- c('longitude', 'latitude')
+
+#str(accident_location_df)
+
+#head(accident_location_df$lonogitude)
+#accident_location_df$longitude <- as.numeric(as.character(accident_location_df$longitude))
+#accident_location_df$latitude <- as.numeric(as.character(accident_location_df$latitude))
+#head(accident_location_df)
+
+#(accident_location_sf <- st_as_sf(accident_location_df, coords = c("longitude", "latitude"), 
+#                   crs = 4326, agr = "constant"))
 
 library("maps")
 states <- st_as_sf(map("state", plot = FALSE, fill = TRUE))
@@ -41,10 +60,156 @@ counties <- subset(counties, grepl("iowa", counties$ID))
 counties$area <- as.numeric(st_area(counties))
 head(counties)
 
+str(accident_location_df)
+accident_location_df$longitude <- as.numeric(accident_location_df$longitude)
+accident_location_df$latitude <- as.numeric(accident_location_df$latitude)
+
+(accident_location_sf <- st_as_sf(accident_location_df, coords = c("longitude", "latitude"), 
+                                  crs = 4326, agr = "constant"))
+
+View(counties)
+library(ggspatial)
+
 ggplot(data = world) +
   geom_sf() +
-#  geom_sf(data = states, fill = NA) + 
-#  geom_sf(data = counties, aes(fill = area)) +
-#  scale_fill_viridis_c(trans = "sqrt", alpha = .4) +
-  geom_sf(data = accident_location_df, size = 3, shape = 23, fill = "darkred") +
+  geom_sf(data = states, fill = NA) + 
+  geom_sf(data = counties, fill = NA) +
+  #geom_sf(data = counties, aes(fill = area)) +
+  scale_fill_viridis_c(trans = "sqrt", alpha = 1) +
+  geom_sf(data = accident_location_sf, size = 1, shape = 23, fill = "red") +
+  geom_sf(data = fatality_locations_sf, size = 2, shape = 21, fill = "yellow") +
+#  geom_point(data = accident_location_df, aes(x = longitude, y = latitude), size = 0.25, shape = 23, fill = "darkred") +
+  coord_sf(xlim = c(-96.724, -90.141), ylim = c(40.410, 43.549), expand = TRUE)
+
+
+
+# Plotting alcohol stores and weather stations
+
+liquor_store_df <- read.csv('C:/Users/Soham More/Documents/GitHub/dmml1/Datasets/Iowa_Liquor_Sales/v1_Data_Processing/Iowa_Liquor_Sales_Final_2015_2016_v2.csv', header = TRUE)
+weather_station_df <- read.csv('C:/Users/Soham More/Documents/GitHub/dmml1/Datasets/Iowa_Weather_Data/NewDatasets/Weather Station Location.csv', header = TRUE)
+head(liquor_store_df)
+head(weather_station_df)
+
+(weather_station_sf <- st_as_sf(weather_station_df, coords = c("Longitude", "Latitude"), 
+                                  crs = 4326, agr = "constant"))
+
+
+liquor_store_location <- liquor_store_df$Store.Location
+head(liquor_store_location)
+liquor_store_location <- sub("\\).*", "", sub(".*\\(", "", liquor_store_location)) 
+head(liquor_store_location)
+liquor_store_location <- unique(liquor_store_location)
+write.csv(liquor_store_location,'C:/Users/Soham More/Documents/GitHub/dmml1/Datasets/Iowa_Liquor_Sales/v1_Data_Processing/unique_liquor_store_locations.csv', row.names = FALSE)
+
+sum(grepl("^\\s*$", liquor_store_location))
+str(liquor_store_location)
+liquor_store_location <- liquor_store_location[!liquor_store_location == '']
+liquor_store_location <- trimws(liquor_store_location)
+liquor_store_location <- gsub("\\s+"," ",liquor_store_location)  # Replace consecutive spaces with a single space
+liquor_store_location <- data.frame(do.call('rbind', strsplit(as.character(liquor_store_location),' ',fixed=TRUE)))
+tail(liquor_store_location)
+colnames(liquor_store_location) <- c('longitude', 'latitude')
+
+
+str(liquor_store_location)
+liquor_store_location$longitude <- as.numeric(as.character(liquor_store_location$longitude))
+liquor_store_location$latitude <- as.numeric(as.character(liquor_store_location$latitude))
+head(liquor_store_location)
+
+(liquor_store_sf <- st_as_sf(liquor_store_location,  coords = c("longitude", "latitude"),
+                                crs = 4326, agr = "constant"))
+
+
+library(ggspatial)
+ggplot(data = world) +
+  geom_sf() +
+  geom_sf(data = states, fill = NA) + 
+  geom_sf(data = counties, fill = NA) +
+  geom_sf(data = counties, aes(fill = area)) +
+  scale_fill_viridis_c(trans = "sqrt", alpha = 1) +
+  geom_sf(data = weather_station_sf, size = 2, shape = 23, fill = "red") +
+  geom_sf(data = liquor_store_sf, size = 2, shape = 21, fill = "yellow") +
+  #geom_label(data = states, aes(X, Y, label = ID), size = 5, fontface = "bold", nudge_y = states$nudge_y) +
+  #annotation_scale(location = "bl", width_hint = 0.2) +
+  #annotation_north_arrow(location = "bl", which_north = "true", 
+  #                       pad_x = unit(0.25, "in"), pad_y = unit(0.25, "in"),
+  #                       style = north_arrow_fancy_orienteering) +
   coord_sf(xlim = c(-96.724, -90.141), ylim = c(40.376, 43.549), expand = TRUE)
+
+
+
+
+
+#####
+## Plot seasonality in alcohol sales
+#####
+
+alcohol_df_for_plotting <- read.csv("C:\\Users\\Soham More\\Documents\\GitHub\\dmml1\\Datasets\\Iowa_Liquor_Sales/v1_Data_Processing/alcohol_df_aggregate.csv", header = TRUE)
+str(alcohol_df_for_plotting)
+alcohol_df_for_plotting <- aggregate(. ~ Date,data=alcohol_df_for_plotting, FUN=sum, na.rm=TRUE, na.action=NULL)
+
+library(ggplot2)
+
+# Basic line plot
+ggplot(data = alcohol_df_for_plotting, aes(x = Date, y = Sale..Dollars., group = 1))+
+  geom_line(color = "#00AFBB", size = 1)
+
+ggplot(data = alcohol_df, aes(x = Date, y = TMIN, group = 1))+
+  geom_line(color = "#00AFBB", size = 1)
+
+
+### Plotting Histogram of temperatures
+
+hist(alcohol_df$TMAX)
+hist(alcohol_df$TMIN)
+
+ggplot(alcohol_df, aes(alcohol_df)) + 
+  geom_histogram(data=subset(alcohol_df,y == 'TMAX'), fill = "red", alpha = 0.2) + 
+  geom_histogram(data=subset(alcohol_df,y == 'TMIN'), fill = "blue", alpha = 0.2) 
+p <- alcohol_df %>%
+ggplot( aes(x=TMAX, fill=type)) +
+  geom_histogram( color="#e9ecef", alpha=0.6, position = 'identity') +
+  scale_fill_manual(values=c("#69b3a2", "#404080")) +
+  theme_ipsum() +
+  labs(fill="")
+
+ggplot(data = alcohol_df) +
+  geom_histogram(aes(x = TMAX, y=(..count..)), 
+                 alpha=0.2, fill ="red",binwidth=2,position="dodge", size =0.5, color = "#FFFFFF") +
+  geom_histogram(aes(x = TMIN, y=(..count..)), 
+                 alpha=0.2,, fill ="blue",binwidth=2,position="dodge", size =0.5, color = "#FFFFFF")
+
+
+# PLotting ROC
+
+library(pROC)
+var1 <- as.numeric(test1$Fatality)
+summary(test1$Fatality)
+table(fitted.results)
+var2 <- as.numeric(fitted.results)
+var3 <- as.numeric(weighted_fitted.results)
+var4 <- as.numeric(naive_pred)
+var5 <- as.numeric(y_pred_svm_linear)
+rocobj_weighted_logit <- roc(var1, var3)
+rocobj_logit <- roc(var1, var2)
+rocobj_naive <- roc(var1, var4)
+rocobj_svm <- roc(var1, var5)
+#rocobj2 <- roc(aSAH$outcome, aSAH$wfns)
+
+library(ggplot2)
+g <- ggroc(rocobj, alpha = 0.5, colour = "red", linetype = 2, size = 2)
+g
+
+g + theme_minimal() + ggtitle("My ROC curve") + 
+  geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), color="grey", linetype="dashed")
+
+
+gl <- ggroc(rocobj, legacy.axes = TRUE)
+gl
+gl + xlab("FPR") + ylab("TPR") + 
+  geom_segment(aes(x = 0, xend = 1, y = 0, yend = 1), color="darkgrey", linetype="dashed")
+
+
+g2 <- ggroc(list(NaiveBayes=rocobj_naive, LogisticRegresssion=rocobj_logit, WeightedLogisticRegression=rocobj_weighted_logit, SVM = rocobj_svm),  size = 1)
+g2  + xlab("FPR") + ylab("TPR") + theme_minimal() + ggtitle("ROC Curve Comparison") + 
+  geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), color="black", linetype="dashed")
